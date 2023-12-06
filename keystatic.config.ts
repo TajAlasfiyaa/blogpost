f89233bea8b68c681469dd3508a70337500b1f6b
TajAlasfiyaa Ishag <tajalasfiyaa@gmail.com>
2023-12-06 07:26:32 +0200

    testing

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, collection, fields } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: {
+      owner: "TajAlasfiyaa",
+      name: "blogpost",
+    },
   },
   collections: {
     posts: collection({
