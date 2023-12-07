360b50fd9c6cff875e3065fdf10a2d4727a8d99a
TajAlasfiyaa Ishag <tajalasfiyaa@gmail.com>
2023-12-07 15:58:00 +0200

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     creator: defaultMetadata.x.creator,
-    creatorId: defaultMetadata.x.creatorId,
+    // creatorId: defaultMetadata.x.creatorId,
     site: defaultMetadata.x.creator,
   },
   robots: {
